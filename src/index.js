d94852b9a639a45e4e88fb5e70de7ef7fca35748
alex1393 <jjbumers@gmail.com>
2023-03-02 23:48:03 -0500

    import redux to prj

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import { createStore } from "redux";
+
 const reducer = (state = 0, action) => {
   switch (action.type) {
     case "INC":
@@ -7,8 +9,12 @@ const reducer = (state = 0, action) => {
   }
 };
 
-let state = reducer(undefined, {});
-state = reducer(state, { type: "INC" });
-console.log(state);
-state = reducer(state, { type: "INC" });
-console.log(state);
+const store = createStore(reducer);
+
+store.subscribe(() => {
+  console.log(store.getState());
+});
+
+console.log(store.getState());
+store.dispatch({ type: "INC" });
+store.dispatch({ type: "INC" });
